12b1ecae356064253e429cc983f39b2d0f3e3b6e
Andreas Schenkel <andreas.accounts@web.de>
2021-12-03 22:12:52 +0100

    v1.0.5

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'report_feedbackchoicegenerator';
-$plugin->release = '1.0.4';
-$plugin->version = 2021120302;
+$plugin->release = '1.0.5';
+$plugin->version = 2021120303;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_RC;
